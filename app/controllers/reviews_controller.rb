379787fbd36aa13ccdf6fe93d011eb5b4b4b4a55
angelo colardo <angelocolardo@gmail.com>
2019-06-05 16:44:38 +0200

    interface works

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,7 +10,7 @@ def new
     @review = Review.new(review_params)
     @review.restaurant = Restaurant.find(params[:restaurant_id])
     if @review.save
-      redirect_to restaurant_path(@review)
+      redirect_to restaurant_path(@review.restaurant)
     else
       render :new
     end
